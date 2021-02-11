5ca21f8925d452a24d75ad2f54d57d268f7788b4
Daniel Buhl <daniel@buhl.nu>
2021-02-11 09:06:38 +0100

    yaml config

--- test_setup/test.js
+++ test_setup/test.js
@@ -6,8 +6,8 @@ const stopwatch = new Stopwatch();
 async function test() {
     stopwatch.start();
 
-    for (var i = 0; i < 1000; i++) {
-        await axios.get("http://localhost:8080");
+    for (var i = 0; i < 10000; i++) {
+        await axios.get("http://172.18.82.247:32140");
     }
     stopwatch.stop()
 
